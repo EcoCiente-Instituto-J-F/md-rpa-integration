b4183b3fcfce1975145b40a7d8dd8e0ca86e152b
shinitihm <heitor.shiniti09@gmail.com>
2026-09-14 09:04:23 -0300

    feat: mapping PK keys

---
+++ src/load/primary_keys.py
@@ -0,0 +1,46 @@
+"""
+Mapeamento das chaves primárias
+das tabelas do banco destino.
+"""
+
+
+PRIMARY_KEYS = {
+
+
+    "usuario":
+
+        "id_usuario",
+
+
+
+    "endereco":
+
+        "id_endereco",
+
+
+
+    "condominio":
+
+        "id_condominio",
+
+
+
+    "cooperativa":
+
+        "id_cooperativa",
+
+
+
+    "material":
+
+        "id_material",
+
+
+
+    "conteudo_educativo":
+
+        "id_conteudo"
+
+
+
+}
