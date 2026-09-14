f31d00563ae628b6c4e75b24d492f62ad1318ce7
shinitihm <heitor.shiniti09@gmail.com>
2026-09-14 08:23:12 -0300

    feat: logging basic config, template: datetime level(info,warning,error) message's

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,12 +1,5 @@
-from loguru import logger
-
-
-logger.add(
-
-"logs/migration.log",
-
-rotation="10 MB",
-
-level="INFO"
-
-)
+import logging, os
+os.makedirs("logs",exist_ok=True)
+def get_logger():
+    logging.basicConfig(filename="logs/migration.log",level=logging.INFO,format="%(asctime)s %(levelname)s %(message)s")
+    return logging.getLogger("migration")
