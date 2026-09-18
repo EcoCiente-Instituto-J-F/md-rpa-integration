4a367c122fa7a82b1be0c7e312885989380ca07e
shinitihm <heitor.shiniti09@gmail.com>
2026-09-18 15:54:53 -0300

    fix: retry handler on insert service

--- src/load/insert_service.py
+++ src/load/insert_service.py
@@ -13,9 +13,9 @@ Responsável por:
 from sqlalchemy import text
 
 from sqlalchemy.exc import SQLAlchemyError
-from src.load.primary_keys import PRIMARY_KEYS
+from load.primary_keys import PRIMARY_KEYS
 from config.logging_config import get_logger
-from src.rpa.retry_handler import RetryHandler
+from rpa.retry_handler import RetryHandler
 
 logger = get_logger()
 
