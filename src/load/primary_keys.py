7b08111635f053f6d4681746eb3fe4c8a883500b
shinitihm <heitor.shiniti09@gmail.com>
2026-09-20 18:39:56 -0300

    feat: defining PKS

--- src/load/primary_keys.py
+++ src/load/primary_keys.py
@@ -5,42 +5,11 @@ das tabelas do banco destino.
 
 
 PRIMARY_KEYS = {
-
-
-    "usuario":
-
-        "id_usuario",
-
-
-
-    "endereco":
-
-        "id_endereco",
-
-
-
-    "condominio":
-
-        "id_condominio",
-
-
-
-    "cooperativa":
-
-        "id_cooperativa",
-
-
-
-    "material":
-
-        "id_material",
-
-
-
-    "conteudo_educativo":
-
-        "id_conteudo"
-
-
-
+    "tb_usuarios": "id_usuario",
+    "tb_enderecos": "id_endereco",
+    "tb_sindicos": "id_sindico",
+    "tb_condominios": "id_condominio",
+    "tb_cooperativas": "id_cooperativa",
+    "tb_materiais": "id_material",
+    "tb_conteudos_educativos": "id_conteudo"
 }
